8bdfafbb76f0482b9a02f01b9d0d080ba9c3e563
cintiamoll34 <mollcintia830@gmail.com>
2022-05-18 15:50:18 -0300

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,4 @@
+let cintia = require ('./cintia_moll')
+
+
+console.log("Hola, soy " + cintia[0] + " me gusta " + cintia[1] + " y vivo en "+ cintia[2])
---
+++ cintia_moll.js
@@ -0,0 +1,6 @@
+
+
+let cintia = [ "Cintia Moll","ver pelis", "San Juan"]; 
+console.log(cintia);
+
+module.exports = cintia; 
